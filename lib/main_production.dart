69672cf7cf1d5ba53cd8fc7623a9ed6c6cb7f209
abdallahyasseinwork <abdallah.work.1998@gmail.com>
2024-03-25 12:08:08 +0200

    -added Development and Production for Android -Changed Android app name for each flavor -added launch.json -added two main files for each flavor

---
+++ lib/main_production.dart
@@ -0,0 +1,17 @@
+import 'package:advanced_project/core/di/dependency_injection.dart';
+import 'package:advanced_project/core/routing/app_router.dart';
+import 'package:advanced_project/doc_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async{
+  setupGetIt();
+  await ScreenUtil.ensureScreenSize();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.amber, // navigation bar color
+    statusBarColor: Colors.green, // status bar color
+  ));
+  runApp( DocApp(appRouter: AppRouter(),));
+}
+
